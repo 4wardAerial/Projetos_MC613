2447330e378db2f69b8bed8855b74833502bad23
leonardo-ferreira-git <l281289@dac.unicamp.br>
2026-03-16 13:49:59 -0300

    bin11_to_bcd4 created and tested

---
+++ P1 - Vending Machine/bin11_to_bcd4.v
@@ -0,0 +1,30 @@
+module bin11_to_bcd4(
+	input wire [10:0] bin,
+	output reg [15:0] bcd
+);
+	integer i;
+	reg [10:0] x;
+	reg [15:0] acc;
+	
+	always @(*) begin
+
+		x = bin;
+		acc = 16'b0;
+		
+		for(i = 10; i >= 0; i = i - 1) begin
+			if(acc[3:0] > 4)
+				acc[3:0] = acc[3:0] + 3;
+			if(acc[7:4] > 4)
+				acc[7:4] = acc[7:4] + 3;
+			if(acc[11:8] > 4)
+				acc[11:8] = acc[11:8] + 3;
+			if(acc[15:12] > 4)
+				acc[15:12] = acc[15:12] + 3;
+			
+			acc = {acc[14:0], x[i]};
+		end
+		
+		bcd = acc;
+		
+	end
+endmodule
