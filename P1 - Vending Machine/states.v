da1610a89d0c2cbb1419c8467fa25488bf81df56
leonardo-ferreira-git <l281289@dac.unicamp.br>
2026-03-16 17:14:03 -0300

    Mudança no gitignore

--- P1 - Vending Machine/states.v
+++ P1 - Vending Machine/states.v
@@ -1,22 +1,31 @@
 module states(
 	input wire clk,
-	output reg current_product,
-	output reg current_shown_value,
-	output reg produto_liberado,
-	output reg troco_devolucao
+	input wire val_prod,
+	input wire val_pag,
+	input wire advance,
+	input wire cancel,
+	output reg [2:0] state
 );
 
-	reg [2:0] state
+	reg [2:0] prev_state;
+	reg [2:0] next_state;
+	
+	c
+	
 	always(@posedge clk) begin
 		case(state)
 			3'b000: begin // A - seleçao do produto
-			
+				if(advance)
+					next_state = 3'b'001
 			end
 			3'b'001: begin // B - inserçao de dinheiro
-			
+				if(val_prod == val_pag)
+					next_state = 3b'010
+				else if(val_prod < val_pag)
+					next_state = 3b'011
 			end
 			3b'010: begin // C - Recebe sem troco
-			
+				
 			end
 			3b'011: begin // D - Recebe com troco
 			
@@ -25,5 +34,7 @@ module states(
 			
 			end	
 		endcase
+		prev_state = state;
+		state = next_state;
 	end
 endmodule
