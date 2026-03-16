c04eca48769cc1e41e549d0718b763e7050e83b2
4wardAerial <4lucas.h.bertanha8@gmail.com>
2026-03-16 16:26:25 -0300

    fixes displayer

--- P1 - Vending Machine/displayer.v
+++ P1 - Vending Machine/displayer.v
@@ -2,24 +2,28 @@ module displayer(
 	input wire [2:0] STATE,
 	input wire [10:0] PROD_VALUE,
 	input wire [10:0] PROD_PRICE,
-	output wire[10:0] PROD_VALUE_BIN,
-	output wire [1:0] LEDR,	// Saida: 2 LEDs vermelhos da placa
+	output reg [10:0] PROD_VALUE_BIN,
+	output reg [1:0] LEDR	// Saida: 2 LEDs vermelhos da placa
 );
 
-if (STATE == 3'b000 || STATE == 3'b001)	// Estado A ou B, nao finalizou compra
-	assign LEDR[0] = 1'b0
-	assign LEDR[1] = 1'b0
-	assign PROD_VALUE_BIN = PROD_VALUE
-end else if (STATE == 3'b010)	// Estado C, recebe sem troco
-	assign LEDR[0] = 1'b1
-	assign LEDR[1] = 1'b0
-	assign PROD_VALUE_BIN = PROD_VALUE
-end else if (STATE == 3'b011) // Estado D, recebe com troco
-	assign LEDR[0] = 1'b1
-	assign LEDR[1] = 1'b1
-	assign PROD_VALUE_BIN = -PROD_VALUE
-end else if (STATE == 3'b100) // Estado E, devolve dinheiro
-	assign LEDR[0] = 1'b0
-	assign LEDR[1] = 1'b1
-	assign PROD_VALUE_BIN = PROD_PRICE - PROD_VALUE
+always @(*) begin
+	if (STATE == 3'b000 || STATE == 3'b001) begin	// Estado A ou B, nao finalizou compra
+		LEDR[0] = 1'b0;
+		LEDR[1] = 1'b0;
+		PROD_VALUE_BIN = PROD_VALUE;
+	end else if (STATE == 3'b010) begin	// Estado C, recebe sem troco
+		LEDR[0] = 1'b1;
+		LEDR[1] = 1'b0;
+		PROD_VALUE_BIN = PROD_VALUE;
+	end else if (STATE == 3'b011) begin	// Estado D, recebe com troco
+		LEDR[0] = 1'b1;
+		LEDR[1] = 1'b1;
+		PROD_VALUE_BIN = -PROD_VALUE;
+	end else if (STATE == 3'b100) begin // Estado E, devolve dinheiro
+		LEDR[0] = 1'b0;
+		LEDR[1] = 1'b1;
+		PROD_VALUE_BIN = PROD_PRICE - PROD_VALUE;
+	end
 end
+
+endmodule
