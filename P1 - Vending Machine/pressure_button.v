10f65187307d602e627844af798a7d16419d1320
Leonardo Ferreira <leoferreira04@outlook.com>
2026-03-20 12:19:04 -0300

    Creation of tb for pressure_button.v

--- P1 - Vending Machine/pressure_button.v
+++ P1 - Vending Machine/pressure_button.v
@@ -1,7 +1,4 @@
-module pressure_button#(
-	parameter CLK = 50_000_000,
-	parameter PRESSURE_TIME = 20
-)
+module pressure_button
 (
 	input wire clk,
 	input wire button_in,
@@ -10,12 +7,8 @@ module pressure_button#(
 	
 );
 	
-	localparam COUNTER_MAX = (CLK / 1000) * PRESSURE_TIME; // Para esperar 20 s
-	localparam COUNTER_WIDTH = $clog2(COUNTER_MAX + 1); // Equação para sabermos quantos bits o registrador tera. Log 2
-	
-	reg [COUNTER_WIDTH - 1 : 0] cont;
-	reg button_sync_0 = 1'b0; // variavel responsável por ser o valor inicial do botão
-	reg button_sync_1 = 1'b0; // botão final do botao
+	reg button_sync_0 = 1'b1; // variavel responsável por ser o valor inicial do botão
+	reg button_sync_1 = 1'b1; // botão final do botao
 	
 	
 
@@ -25,16 +18,10 @@ module pressure_button#(
 	end
 	
 	always @(posedge clk) begin
-		if(button_sync_1 != button_out) begin
-			if(cont < COUNTER_MAX) begin
-				cont <= cont + 1'b1;
-			end else begin
-				button_out <= button_sync_1;
-				cont <= 0;
-			end
+		if(button_sync_1 == 1 && button_sync_0 == 0) begin
+			button_out <= 1;
 		end else begin
-			cont <= 0;
-			
-			end
+			button_out <= 0;
 		end
-	endmodule
+	end
+endmodule
