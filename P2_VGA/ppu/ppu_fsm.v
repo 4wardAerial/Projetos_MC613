e04bde31e79e0dfecd4b155744141923e5a43ede
Rafael Tagliamento Santana <ra281359@santos.lab.ic.unicamp.br>
2026-03-30 17:23:54 -0300

    State machine for ppu completed

--- P2_VGA/ppu/ppu_fsm.v
+++ P2_VGA/ppu/ppu_fsm.v
@@ -1,6 +1,7 @@
 module ppu_fsm (
     input wire clk,
     input wire rst_n,
+    input wire oam_ready,
     input wire [8:0] pixel_x,
     input wire [8:0] pixel_y,
     output reg [1:0] state
@@ -9,12 +10,29 @@ module ppu_fsm (
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             state <= 2'b00; 
-        end else begin
-            case (state)
-                2'b00: state <= 2'b01; 
-                2'b01: state <= 2'b10; 
-                2'b10: state <= 2'b00; 
-                default: state <= 2'b00;
-            endcase
+        end 
+        
+        else if (state == 2'b00 && oam_ready) begin
+            state <= 2'b01; 
+        end
+
+        else if (state == 2'b01 && pixel_x == 640) begin
+            state <= 2'b10; 
+        end        
+            
+        else if (state == 2'b10) begin
+            if (pixel_x == 800) begin
+                if (pixel_y == 480) begin
+                    state <= 2'b11; 
+                end else begin
+                    state <= 2'b01; 
+                end 
+            end
+        end
+
+        else if (state == 2'b11) begin
+            if (pixel_y == 525) begin
+                state <= 2'b00; 
+            end
         end
     end
