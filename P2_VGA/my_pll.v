2bb63ab06d2b3ebb93f2037971c3fbf4166f397b
Vinícius Maciel de Sousa <v281391@dac.unicamp.br>
2026-03-30 16:59:28 -0300

    Criado pll

---
+++ P2_VGA/my_pll.v
@@ -0,0 +1,18 @@
+module my_pll(clk_in, rstn, pll_clk_out_25mhz,
+					pll_clk_out_150mhz);
+					
+input clk_in;
+input rstn;
+output pll_clk_out_25mhz;
+output pll_clk_out_150mhz;
+
+
+pll_ip pll_ip_inst (
+		.refclk   (clk_in),   //  refclk.clk
+		.rst      (!rstn),      //   reset.reset
+		.outclk_0 (pll_clk_out_25mhz), // outclk0.clk
+		.outclk_1 (pll_clk_out_150mhz), // outclk1.clk
+		.locked   ()    //  locked.export
+	);
+	
+endmodule
---
+++ P2_VGA/my_pll_tb.v
@@ -0,0 +1,3 @@
+//time_scale = 1ns/1ps
+
+//module 
