de2ab66e60845ffe6583bacce469a9c5b8b6695d
leonardo-ferreira-git <l281289@dac.unicamp.br>
2026-04-06 16:47:29 -0300

    ROM do background

---
+++ P2_VGA/bg_ROM.v
@@ -0,0 +1,14 @@
+module bg_ROM (
+	input wire [3:0] linha,
+	input wire [3:0] coluna
+	output wire [7:0] data_out
+);
+
+reg [3:0] mem [0:255]
+
+initial begin
+	$readmemh("bckg.txt", mem);
+end
+
+assign data_out = mem[linha][coluna]
+end
