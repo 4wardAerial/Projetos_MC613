b1d815ce6962a775530dd4315cf76fddc7b84b3a
leonardo-ferreira-git <l281289@dac.unicamp.br>
2026-04-06 16:48:12 -0300

    Correção

--- P2_VGA/bg_ROM.v
+++ P2_VGA/bg_ROM.v
@@ -1,14 +1,14 @@
 module bg_ROM (
 	input wire [3:0] linha,
-	input wire [3:0] coluna
+	input wire [3:0] coluna,
 	output wire [7:0] data_out
 );
 
-reg [3:0] mem [0:255]
+reg [3:0] mem [0:255];
 
 initial begin
 	$readmemh("bckg.txt", mem);
 end
 
-assign data_out = mem[linha][coluna]
+assign data_out = mem[linha][coluna];
 end
