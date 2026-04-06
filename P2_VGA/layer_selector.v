ee84783040ee61e5ba470dc5f9de7c596dd58eae
4wardAerial <4lucas.h.bertanha8@gmail.com>
2026-04-06 16:53:40 -0300

    corrige tamanho cor

--- P2_VGA/layer_selector.v
+++ P2_VGA/layer_selector.v
@@ -1,8 +1,8 @@
 module layer_selector(
-	input wire [5:0] bckg_color,
-	input wire [5:0] sprite_color,
+	input wire [24:0] bckg_color,
+	input wire [24:0] sprite_color,
 	input wire transparent,
-	output wire [5:0] layer_color
+	output wire [24:0] layer_color
 );
 
 assign layer_color = transparent ? bckg_color : sprite_color;
