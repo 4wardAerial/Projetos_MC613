ab70880d69eaed01a469de246ef1b87d5ece4d2a
Rafael Tagliamento Santana <taglirafael@gmail.com>
2026-04-08 15:08:15 -0300

    Update sprite module to use 10-bit coordinates

--- P2_VGA/find_sprites_ppu.v
+++ P2_VGA/find_sprites_ppu.v
@@ -1,36 +1,49 @@
 module find_sprites (
-        input wire clk,
-        input wire rst_n,
-        input wire [8:0] pixel_x,
-        input wire [8:0] pixel_y,
-        output reg [0:0] ID,
-        output reg [1:0] tile_x,
-        output reg [1:0] tile_y
-    );
+    input wire clk,
+    input wire rst_n,
+    input wire [9:0] pixel_x,
+    input wire [9:0] pixel_y,
+    output reg [0:0] ID,
+    output reg [1:0] tile_x,
+    output reg [1:0] tile_y
+);
 
-    reg [5:0] sprite [0:1]; 
-	integer i;
+    reg [23:0] sprite [0:1]; 
 	 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            for (i = 0; i < 2; i = i + 1) begin
-                if (pixel_x <= sprite[i][0:1] + 8*sprite[i][4] && 
-                    pixel_y <= sprite[i][2:3] + 8*sprite[i][5]) begin
+            ID <= 0;
+            tile_x <= 0;
+            tile_y <= 0;
+        end else begin
+            if (pixel_x >= (sprite[0][23:16] * 8) && 
+                pixel_x <  ((sprite[0][23:16] + sprite[0][7:4]) * 8) &&
+                pixel_y >= (sprite[0][15:8] * 8) && 
+                pixel_y <  ((sprite[0][15:8] + sprite[0][3:0]) * 8)) begin
+                
+                ID <= 0;
+                tile_x <= (pixel_x - (sprite[0][23:16] * 8)) / 8;
+                tile_y <= (pixel_y - (sprite[0][15:8] * 8)) / 8;
 
-                    ID <= i;
-                    tile_x <= (pixel_x - sprite[i][0:1]) / 8;
-                    tile_y <= (pixel_y - sprite[i][2:3]) / 8;
-                    break;
-                end
+            end else if (pixel_x >= (sprite[1][23:16] * 8) && 
+                         pixel_x <  ((sprite[1][23:16] + sprite[1][7:4]) * 8) &&
+                         pixel_y >= (sprite[1][15:8] * 8) && 
+                         pixel_y <  ((sprite[1][15:8] + sprite[1][3:0]) * 8)) begin
+                
+                ID <= 1;
+                tile_x <= (pixel_x - (sprite[1][23:16] * 8)) / 8;
+                tile_y <= (pixel_y - (sprite[1][15:8] * 8)) / 8;
+                
+            end else begin
+                ID <= 0;
+                tile_x <= 0;
+                tile_y <= 0;
             end
         end
     end
 
-            
-    initial
-	 begin
-        sprite[0] = 6'h040533;
-        sprite[1] = 6'h090533; //tile 09, tile 05, size 3x3 tiles
+    initial begin
+        sprite[0] = 24'h040533; // Correção de tamanho
+        sprite[1] = 24'h090533; 
     end
-
 endmodule
